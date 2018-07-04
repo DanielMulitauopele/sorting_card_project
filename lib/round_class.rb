4fc2c46f21535ca578b48da38f2ca6d11985ebc4
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-07-03 19:06:25 -0600

    adds methods for guess tests

--- lib/round_class.rb
+++ lib/round_class.rb
@@ -1,26 +1,34 @@
 require './lib/card_class.rb'
 require './lib/guess_class.rb'
 require './lib/deck_class.rb'
+require 'pry'
 
 class Round
   attr_reader :deck
 
   def initialize(deck)
     @deck = deck
+    @guesses = []
+    @new_response = ""
+    @correct_guesses = 0
   end
 
   def guesses
+    @guesses
   end
 
   def current_card
+    @current_card = deck.cards.first
   end
 
-  def record_guess(guess)
+  def record_guess(hash)
+    @new_response = "#{hash[:value]} of #{hash[:suit]}"
+    new_guess = Guess.new(@new_response, @current_card)
   end
 
-  def number_correct
-  end
+  # def number_correct
+  # end
 
-  def percent_correct
-  end
+  # def percent_correct
+  # end
 end
